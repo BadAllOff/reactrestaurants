71e512da4fca93859d31c26947d9afe89042452b
Shamil <badalloffsh@gmail.com>
2017-11-11 23:28:38 +0100

    Add: ancestry to models and foundation-rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171106142908) do
+ActiveRecord::Schema.define(version: 20171111211926) do
 
   create_table "comments", force: :cascade do |t|
     t.string "author"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20171106142908) do
     t.integer "restaurant_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_comments_on_ancestry"
     t.index ["restaurant_id"], name: "index_comments_on_restaurant_id"
   end
 
