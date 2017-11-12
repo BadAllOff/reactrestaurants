595196191acf27d6dec9ebfc45ac5c51b0f0180c
Shamil <badalloffsh@gmail.com>
2017-11-13 00:37:26 +0100

    Add: maping into comment component

--- app/assets/javascripts/components/comment_list.js.jsx
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -3,10 +3,8 @@ var CommentList = createReactClass({
         return(
           <div>
             {JSON.parse(this.props.comments).map(function(comment) {
-                return <Comment rank={comment.rank}
-                                body={comment.body}
-                                author={comment.author}
-                                key={comment.id}/>;
+                // Take comment and map it to the Comment component
+                return <Comment key={comment.id} {... comment}/>;
             })}
           </div>
         );
