1a0d89bd316f61ef3a5e35a89dae855989778f90
Shamil <badalloffsh@gmail.com>
2017-11-29 21:43:55 +0100

    Add: description comments

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,6 +3,14 @@ var Constants = {
     ADD_COMMENT: 'comments.add'
 }
 
+// Store responsible to inform the universe of components
+// that there is in fact a change.
+// It removed from react components and lives in this Store concept
+// Thats why we have addChangeListener, removeChangeListener methods.
+// Components will add\remove themselves ass listeners to the Store
+// Likewise, the Store needs a method for emitting those changes
+// to everybody that cares - emitChange
+
 var Store = new _.extend({}, EventEmitter.prototype, {
     _comments: [],
 
@@ -12,7 +20,9 @@ var Store = new _.extend({}, EventEmitter.prototype, {
 
     comments: function () {
         return this._comments;
-    }
+    },
+
+    // Event emmiter - central point of messaging
 
     addChangeListener: function (callback) {
         this.on(Constants.CHANGE_EVENT, callback)
@@ -25,18 +35,23 @@ var Store = new _.extend({}, EventEmitter.prototype, {
     emitChange: function () {
         this.emit(Constants.CHANGE_EVENT);
     }
-
 });
 
-var AppDispatcher = new FluxDispatcher();
 
+// In Flux application there should be only one central dispatcher(controller)
+var AppDispatcher = new Flux.Dispatcher();
+
+// This is where we "marry" the actions, that our app can perform
+// with the date, that is going to be changed
 AppDispatcher.register(function (payload) {
+    //
     var action = payload.actionType;
     switch(action) {
+        //case of our action names
         case Constants.ADD_COMMENT:
             Store.addComment(payload.comment);
             break;
         default:
         // NO_OP
     }
-})
+});
